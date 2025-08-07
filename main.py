b73fa38ccb48ba88987515acf476560676a5d7ec
Luca Maurici <Luca Maurici>
2025-08-07 20:59:57 +0200

    Aggiunto troppo alto o troppo basso

--- main.py
+++ main.py
@@ -4,18 +4,15 @@ numero_segreto = random.randint(1, 5)
 
 print("\nIndovina il numero tra 1 e 5!\n")
 
-da_indovinare = True
-
-while da_indovinare == True:
+while True:
     risposta = int(input("Il tuo tentativo: "))
 
-    if numero_segreto == risposta:
-        print("\nRisposta corretta!")
-        da_indovinare = False
+    if risposta < numero_segreto:
+        print("Troppo basso!")
+    elif risposta > numero_segreto:
+        print("Troppo alto!")
     else:
-        print("\nRisposta sbagliata!")
-else:
-    print("Fine del gioco, ciao!")
+        print("\nRisposta corretta!")
+        break
 
-
-
+print("Fine del gioco, ciao!")
