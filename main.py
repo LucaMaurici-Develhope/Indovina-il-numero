f0a0bcea2967f42d17d353142f989d7a3fe26416
Luca Maurici <Luca Maurici>
2025-08-07 20:51:26 +0200

    Feature Rigioca implementata

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-numero_segreto = 4
+import random
 
-print("\nIndovina il numero tra 1 e 10!\n")
+numero_segreto = random.randint(1, 5)
 
-risposta = input("Il tuo tentativo: ")
+print("\nIndovina il numero tra 1 e 5!\n")
 
-risposta = int(risposta)
+da_indovinare = True
 
-if numero_segreto == risposta:
-    print("\nRisposta corretta!")
+while da_indovinare == True:
+    risposta = int(input("Il tuo tentativo: "))
+
+    if numero_segreto == risposta:
+        print("\nRisposta corretta!")
+        da_indovinare = False
+    else:
+        print("\nRisposta sbagliata!")
 else:
-    print("\nRisposta sbagliata!")
+    print("Fine del gioco, ciao!")
+
+
 
