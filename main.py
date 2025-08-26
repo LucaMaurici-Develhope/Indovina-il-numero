3de00349a4da15886aaf9a897f0dc61b8232024e
Luca Maurici <Luca Maurici>
2025-08-26 21:00:04 +0200

    Ora il gioco termina se non si indovina entro 5 tentativi

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import random
 
-numero_segreto = random.randint(1, 5)
+tentativi_rimasti = 5
 
-print("\nIndovina il numero tra 1 e 5!\n")
+numero_segreto = random.randint(1, 10)
 
-while True:
+print("\nIndovina il numero tra 1 e 10!\n")
+
+while tentativi_rimasti > 0:
+
+    print(f"Rimangono {tentativi_rimasti} tentativi.")
     risposta = int(input("Il tuo tentativo: "))
 
     if risposta < numero_segreto:
@@ -12,7 +16,9 @@ while True:
     elif risposta > numero_segreto:
         print("Troppo alto!")
     else:
-        print("\nRisposta corretta!")
+        print("\nRisposta corretta! Hai vinto!")
         break
 
-print("Fine del gioco, ciao!")
+    tentativi_rimasti -= 1
+else:
+    print("\nHai perso! Il numero era:", numero_segreto)
