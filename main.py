465de4c4de0fdd739a0ed8a51c389145f708b3a4
vavalu-ai <mavavaluva@gmail.com>
2025-09-02 19:12:02 +0200

    Aggiunta difficoltà (tentativi_max) by Diego

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import random
-
-tentativi_rimasti = 5
-
+print ("seleziona difficoltà") 
+print("1: Facile: numero di tentativi= 5")
+print("2: Difficile: numero di tentativi= 2")
+risposta= input ("seleziona difficoltà")
+if risposta== "1": 
+    print("hai selezionato facile")
+    tentativi_rimasti=5
+elif risposta== "2": 
+    print("hai selezionato difficile")
+    tentativi_rimasti=2
 numero_segreto = random.randint(1, 10)
+
+
 
 print("\nIndovina il numero tra 1 e 10!\n")
 
